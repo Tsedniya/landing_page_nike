68c387ecff3867c8b6a6e678fb5e48d09443d0c0
Tsedniya <ktsedniya@gmail.com>
2025-07-11 21:53:34 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-red-500 font-bold text-center'>hello</h1>
+     
+    </>
+  )
+}
+
+export default App
