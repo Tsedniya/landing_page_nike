396eeb70d2833222e1f2f40b61145a9b5a1901f8
Tsedniya <ktsedniya@gmail.com>
2025-07-13 22:14:58 +0300

    Your message

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ function App() {
 
   return (
     <>
-      <h1 className='text-red-500 font-bold text-9xl'>hello</h1>
+      <h1 className='text-red-500 font-bold text-9xl'>hello world</h1>
+      <button className='text-amber-200'>now</button>
      
     </>
   )
