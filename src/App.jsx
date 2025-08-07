f7f64c15effdfee32c45be6d9110f599385f8454
Tsedniya <ktsedniya@gmail.com>
2025-08-07 20:53:21 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,46 @@ import { useState } from 'react'
 
 import './index.css'
 
-function App() {
+import React from 'react'
 
-
+const App = () => {
   return (
-    <>
-      <h1 className='text-red-500 font-bold text-9xl'>hello world</h1>
-      <button className='text-amber-200'>now</button>
-     
-    </>
+    <main className='relative'>
+      nav
+    <section  className='xl:padding-1 wide:padding-r padding-b'>
+       hero
+    </section>
+    <section className='padding'> 
+       Popularproducts
+    </section>
+      <section className='padding'> 
+       superquality
+    </section>
+      <section className='padding-x py-10'> 
+       services
+    </section>
+    <section className='padding'> 
+        specialOffer
+    </section>
+    <section className='bg-pale-blue padding'> 
+        customerreviews
+    </section>
+    <section className='padding-x sm:py-32 py-16 w-full'> 
+        subscribe
+    </section>
+     <section className='padding-x sm:py-32 py-16 w-full'> 
+        experince
+    </section>
+    <section className='bg-black padding-x padding-t pb-8'> 
+        footer
+    </section>
+
+        
+        
+    </main>
   )
 }
 
-export default App
+export default App;
+
+
